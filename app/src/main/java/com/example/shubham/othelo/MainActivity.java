ef73d3f5614fba83d6c18fa94e99be48167ff578
sgoyal12 <shu12.sg@gmail.com>
2018-06-23 13:23:49 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/shubham/othelo/MainActivity.java
@@ -0,0 +1,99 @@
+package com.example.shubham.othelo;
+
+import android.graphics.Canvas;
+import android.graphics.ColorFilter;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.GradientDrawable;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements  View.OnClickListener {
+LinearLayout rootlayout;
+    ArrayList<LinearLayout> rows;
+    public OButton[][] board;
+    public static  final int size=8;
+    public static int currentStatus;
+    public static final int INCOMPLETE=0,BLACK_WON=1,WHITE_WON=2;
+    public static final int BLACK=0,WHITE=1,NO_PLAYER=2;
+    public static int currentPlayer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        rootlayout=findViewById(R.id.RootLayout);
+        setBoard();
+    }
+
+    public void setBoard() {
+        board=new OButton[size][size];
+        rows=new ArrayList<>();
+        currentStatus=INCOMPLETE;
+        currentPlayer=BLACK;
+        rootlayout.removeAllViews();
+        for(int i=0;i<size;i++)
+        {
+            LinearLayout linearLayout=new LinearLayout(this);
+            linearLayout.setOrientation(LinearLayout.HORIZONTAL);
+            LinearLayout.LayoutParams layoutParams=new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,0,1);
+            linearLayout.setLayoutParams(layoutParams);
+            rootlayout.addView(linearLayout);
+
+            rows.add(linearLayout);
+        }
+        for(int i=0;i<size;i++)
+        {
+            for (int j=0;j<size;j++)
+            {
+                OButton button=new OButton(this);
+                LinearLayout.LayoutParams layoutParams=new LinearLayout.LayoutParams(0, LinearLayout.LayoutParams.MATCH_PARENT,1);
+                button.setLayoutParams(layoutParams);
+
+                button.setBackgroundDrawable(getResources().getDrawable(R.drawable.buttonbg));
+                button.setTextSize(10);
+                button.setOnClickListener(this);
+                LinearLayout row=rows.get(i);
+                row.addView(button);
+                button.setXY(i,j);
+                board[i][j]=button;
+            }
+        }
+        board[3][3].setPlayer(BLACK,board,size);
+        board[3][4].setPlayer(WHITE,board,size);
+        board[4][3].setPlayer(WHITE,board,size);
+        board[4][4].setPlayer(BLACK,board,size);
+
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        if(currentStatus==INCOMPLETE) {
+            OButton button = (OButton) v;
+            button.setPlayer(currentPlayer,board,size);
+
+
+            setGameStatus();
+            tooglePlayer();
+        }
+    }
+
+    private void tooglePlayer() {
+        if(currentPlayer==BLACK)
+            currentPlayer=WHITE;
+        else {
+            currentPlayer=BLACK;
+        }
+    }
+
+    private void setGameStatus() {
+
+    }
+
+}
---
+++ app/src/main/java/com/example/shubham/othelo/OButton.java
@@ -0,0 +1,150 @@
+package com.example.shubham.othelo;
+
+import android.content.Context;
+import android.graphics.drawable.GradientDrawable;
+import android.widget.Button;
+
+/**
+ * Created by shubham on 6/22/2018.
+ */
+
+public class OButton extends android.support.v7.widget.AppCompatButton {
+   private int player=MainActivity.NO_PLAYER;
+   private  int x,y;
+   private boolean validStatus=false;
+   public int blackstatus=0,whitestatus=0;
+
+   public OButton(Context context) {
+        super(context);
+    }
+    public void setXY(int i,int j)
+    {
+        x=i;
+        y=j;
+    }
+    public void setPlayer(int currentPlayer,OButton board[][],int size) {
+        this.player=currentPlayer;
+        blackstatus=0;
+        whitestatus=0;
+        if(player==MainActivity.BLACK){
+            setBackgroundDrawable(getResources().getDrawable(R.drawable.buttonb));
+            System.out.println(MainActivity.BLACK);
+            aroundBlackStatus(board,size);
+        }
+        else if(player==MainActivity.WHITE){
+           setBackgroundDrawable(getResources().getDrawable(R.drawable.buttonw));
+            System.out.println(MainActivity.WHITE);
+            aroundWhiteStatus(board,size);
+        }
+        setEnabled(false);
+        checkValidStatus(currentPlayer,board,size);
+        setValidStatus(board,size);
+
+    }
+
+
+
+    private void aroundWhiteStatus(OButton[][] board, int size) {
+        int  i,j;
+        for(i=this.x-1;i<this.x+2;i++)
+        {
+            for (j=this.y-1;j<this.y+2;j++)
+            {
+                if(i>=0&&i<size&&j>=0&&j<size&&board[i][j].isEmpty())
+                    board[i][j].whitestatus++;
+
+            }
+        }
+    }
+
+    private void aroundBlackStatus(OButton[][] board, int size) {
+        int  i,j;
+        for(i=this.x-1;i<this.x+2;i++)
+        {
+            for (j=this.y-1;j<this.y+2;j++)
+            {
+                if(i>=0&&i<size&&j>=0&&j<size&&board[i][j].isEmpty())
+                    board[i][j].blackstatus++;
+
+            }
+        }
+    }
+
+
+    public void checkValidStatus(int currentPlayer, OButton[][] board, int size) {
+        for(int i=0;i<size;i++)
+        {
+            for(int j=0;j<size;j++)
+            {
+              int status,otherPlayer,a;
+              if(currentPlayer==MainActivity.BLACK){
+                  status=board[i][j].blackstatus;
+                  otherPlayer=MainActivity.WHITE;
+              }
+              else {
+                  status=board[i][j].whitestatus;
+                  otherPlayer=MainActivity.BLACK;
+              }
+              if(status!=0) {
+                  for (int k = 1; j-k >=0; k++) {
+                        if(board[i][j-k].isEmpty()||(board[i][j-k].player==otherPlayer&&k==1)) {
+                            a=0;
+                            break;
+                        }
+                        else if(board[i][j].player==otherPlayer)
+                        {
+                          a=1;
+                          break;
+                        }
+                  }
+                  for (int k = 1; k < size; k++) {
+
+                  }
+                  for (int k = 1; k < size; k++) {
+
+                  }
+                  for (int k = 1; k < size; k++) {
+
+                  }
+                  for (int k = 1; k < size; k++) {
+
+                  }
+                  for (int k = 1; k < size; k++) {
+
+                  }
+                  for (int k = 1; k < size; k++) {
+
+                  }
+                  for (int k = 1; k < size; k++) {
+
+                  }
+              }
+            }
+        }
+    }
+
+    private void setValidStatus( OButton[][] board, int size) {
+        for(int i=0;i<size;i++)
+        {
+            for(int j=0;j<size;j++)
+            {
+                if(board[i][j].validStatus){
+                    board[i][j].validStatus=false;
+                    if(board[i][j].isEmpty())
+                    {
+                        board[i][j].setText(".");
+                    }
+                }
+
+            }
+        }
+    }
+    public boolean isEmpty()
+    {
+        return this.getPlayer()==MainActivity.NO_PLAYER;
+    }
+
+    private int getPlayer() {
+        return this.player;
+    }
+}
