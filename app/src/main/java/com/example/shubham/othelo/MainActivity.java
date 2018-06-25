69db97be5bd13b76d272a93425e08f8b2c0979bb
sgoyal12 <shu12.sg@gmail.com>
2018-06-25 10:38:45 +0530

    added menu

--- app/src/main/java/com/example/shubham/othelo/MainActivity.java
+++ app/src/main/java/com/example/shubham/othelo/MainActivity.java
@@ -8,6 +8,8 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -36,7 +38,18 @@ LinearLayout rootlayout;
         tvw=findViewById(R.id.whiteTiles);
         setBoard();
     }
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu,menu);
 
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (item.getItemId() == R.id.RESET)
+            setBoard();
+        return true;
+    }
     public void setBoard() {
         board=new OButton[size][size];
         rows=new ArrayList<>();
